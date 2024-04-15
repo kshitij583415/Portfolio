b68d8e3aa7f48e624dd5eb3c8ddc5539b9dcf28d
kshitij583415 <01fe21bcs314@kletech.ac.in>
2024-04-15 19:46:28 +0530

    portfolio

--- src/content_option.js
+++ src/content_option.js
@@ -1,52 +1,52 @@
-const logotext = "JOHN";
+const logotext = "Kshitij";
 const meta = {
-    title: "John Doe",
-    description: "I’m John Doe data scientist _ Full stack devloper,currently working in Berlin",
+    title: "Kshitij",
+    description: "I’m Kshitij Full stack web devloper,currently studying in Hubli",
 };
 
 const introdata = {
-    title: "I’m John Doe",
+    title: "I’m Kshitij",
     animated: {
         first: "I love coding",
         second: "I code cool websites",
-        third: "I develop mobile apps",
+        third: "I attend all the classes and have 100% in webd",
     },
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum",
+    description: "My name is  Kshitij i m currently persuing BE from KLE Technological Univsersity.",
     your_img_url: "https://images.unsplash.com/photo-1514790193030-c89d266d5a9d",
 };
 
 const dataabout = {
     title: "abit about my self",
-    aboutme: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent quis dolor id ligula semper elementum feugiat pretium nulla. Nunc non commodo dolor. Nunc mollis dignissim facilisis. Morbi ut magna ultricies.",
+    aboutme: "My name is Kshitij i m currently persuing BE from KLE Technological Univsersity.I'm a dedicated web developer with a passion for coding. Crafting digital experiences is not just my profession; it's where my enthusiasm lies. With a strong background in web development, I bring creativity and technical expertise to every project. I thrive on the challenges of coding and find joy in building seamless, user-friendly websites. Let's turn ideas into reality through the language of the web.",
 };
 const worktimeline = [{
-        jobtitle: "Designer of week",
-        where: "YAdfi",
+        jobtitle: "Software Developer3",
+        where: "Banglore",
         date: "2020",
     },
     {
-        jobtitle: "Designer of week",
-        where: "Jamalya",
+        jobtitle: "Software Developer2",
+        where: "Pune",
         date: "2019",
     },
     {
-        jobtitle: "Designer of week",
-        where: "ALquds",
-        date: "2019",
+        jobtitle: "Software Developer",
+        where: "Delhi",
+        date: "2018",
     },
 ];
 
 const skills = [{
         name: "Python",
+        value: 60,
+    },
+    {
+        name: "CPP",
         value: 90,
     },
     {
-        name: "Djano",
-        value: 85,
-    },
-    {
         name: "Javascript",
-        value: 80,
+        value: 90,
     },
     {
         name: "React",
@@ -54,7 +54,15 @@ const skills = [{
     },
     {
         name: "Jquery",
+        value: 30,
+    },
+    {
+        name: "Nodejs",
         value: 85,
+    },
+    {
+        name: "Mongodb",
+        value: 79,
     },
 ];
 
@@ -63,82 +71,67 @@ const services = [{
         description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
     },
     {
-        title: "Mobile Apps",
+        title: "Backend Developer",
         description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
     },
     {
-        title: "Wordpress Design",
+        title: "React Developer",
         description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
     },
 ];
 
 const dataportfolio = [{
-        img: "https://picsum.photos/400/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
+        img: "https://play-lh.googleusercontent.com/4uaEj3JqtzKdMYei48166qVSHpaGH2xZE5oYz7MVHlL0ooGHnLZmCBIR_kZT7ytnKQ",
+        description: "Your instant gateway to seamless and expressive conversations. Connect with ease, share moments, and make every chat memorable.",
         link: "#",
     },
     {
-        img: "https://picsum.photos/400/800/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
+        img: "https://i.pinimg.com/originals/53/8c/32/538c32489d4b9ea44ff34f63e41ca14c.png",
+        description: "Experience weather at your fingertips with our Weather App. Stay informed with real-time forecasts, personalized weather updates, and a user-friendly interface that brings the latest conditions right to your device.",
         link: "#",
     },
     {
-        img: "https://picsum.photos/400/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
+        img: "https://th.bing.com/th/id/OIP.WqJLkKbRNWOH-FguxGnVTQHaHa?w=192&h=192&c=7&r=0&o=5&dpr=1.3&pid=1.7",
+        description: "Discover curated products and seamless shopping on our E-commerce Website. Elevate your online shopping experience with secure transactions and personalized recommendations.",
         link: "#",
     },
     {
-        img: "https://picsum.photos/400/600/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
+        img: "https://th.bing.com/th/id/OIP.bf_Um5shxy9Snj8HbRR0ZAHaHa?rs=1&pid=ImgDetMain",
+        description: "Boost your productivity with our Todo List app – your ultimate task management companion. Effortlessly organize your day, set priorities, and track your accomplishments, ensuring you stay focused and on top of your goals.",
         link: "#",
     },
     {
-        img: "https://picsum.photos/400/300/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
+        img: "https://th.bing.com/th/id/OIP.XEycAeqBiRMbExrzSgUgPwHaHN?w=188&h=183&c=7&r=0&o=5&dpr=1.3&pid=1.7",
+        description: "Unlock hassle-free scheduling and convenience with our Booking App. Seamlessly book appointments, services, or events, all in one place, providing you with a streamlined and efficient way to manage your reservations effortlessly.",
         link: "#",
     },
     {
-        img: "https://picsum.photos/400/700/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
+        img: "https://webstockreview.net/images/life-clipart-quality-life-2.png",
+        description: "Elevating quality of life through our EDA project, we harness data insights to enhance well-being, foster sustainable practices, and create a positive impact on communities.",
         link: "#",
     },
 
     {
-        img: "https://picsum.photos/400/600/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
+        img: "https://www.admissionfever.com/Media/clgimg/gallery/44384_img8167798265878378.jpg",
+        description: "I enhanced a DUGC website tailored for use by college coordinators and chairman. The website features functionalities such as ISA and lab analysis, circular eligibility checks, and course withdrawal options. ",
         link: "#",
     },
-    {
-        img: "https://picsum.photos/400/300/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
-    {
-        img: "https://picsum.photos/400/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
-    {
-        img: "https://picsum.photos/400/550/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
-    {
-        img: "https://picsum.photos/400/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
-    {
-        img: "https://picsum.photos/400/700/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
+    // {
+    //     img: "https://picsum.photos/400/300/?grayscale",
+    //     description: "The wisdom of life consists in the elimination of non-essentials.",
+    //     link: "#",
+    // },
+    // {
+    //     img: "https://picsum.photos/400/?grayscale",
+    //     description: "The wisdom of life consists in the elimination of non-essentials.",
+    //     link: "#",
+    // },
 ];
 
 const contactConfig = {
-    YOUR_EMAIL: "name@domain.com",
-    YOUR_FONE: "(555)123-4567",
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vehicula eu nunc et sollicitudin. Cras pulvinar, nisi at imperdiet pharetra. ",
+    YOUR_EMAIL: "kshitij2k04@gmail.com",
+    YOUR_FONE: "9555402098",
+    description: "Feel free to reach out! Whether you have a project in mind, want to discuss collaboration opportunities, or simply have a question, I'm always open to connecting with fellow enthusiasts, clients, or anyone with a shared interest in the world of web development. Your message is just a click away, and I look forward to hearing from you soon. Let's bring ideas to life and create something amazing together ",
     // creat an emailjs.com account 
     // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
     YOUR_SERVICE_ID: "service_id",
@@ -149,7 +142,7 @@ const contactConfig = {
 const socialprofils = {
     github: "https://github.com",
     facebook: "https://facebook.com",
-    linkedin: "https://linkedin.com",
+    linkedin: "https://www.linkedin.com/in/Kshitij-kanaujia-787872269/",
     twitter: "https://twitter.com",
 };
 export {
